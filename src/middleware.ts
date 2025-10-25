46dac9dc2a33c8aafe8e4dfd613cf2ae0a5a5b82
Vedanttttt05 <vedantpatil05052004@gmail.com>
2025-10-25 22:59:44 +0530

    fix middleware redirect for users who are not signed in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { NextResponse   } from 'next/server'
 import type { NextRequest } from 'next/server'
 import {getToken} from "next-auth/jwt"
+import { ur } from 'zod/locales'
  
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     const token = await getToken({req : request})
     const url = request.nextUrl
@@ -10,9 +10,12 @@ export async function middleware(request: NextRequest) {
         ((url.pathname.startsWith('/sign-in')) ||
         (url.pathname.startsWith('/sign-up')) ||
         (url.pathname.startsWith('/verify')))
-    ){return NextResponse.redirect(new URL('/home', request.url))
 
+
+    ){return NextResponse.redirect(new URL('/home', request.url))
     }
+    if (!token &&  url.pathname.startsWith('/dashboard')){
+        return NextResponse.redirect(new URL('/sign-in', request.url))
   return NextResponse.next()
 }
  
